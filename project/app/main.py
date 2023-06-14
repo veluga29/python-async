b90a31567d94ab906cd18797a30d162ef4e26460
lucian <sungyoon606@naver.com>
2023-06-14 09:37:08 +0900

    Modify host, port setting and change jinja template base url form

--- project/app/main.py
+++ project/app/main.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent
 app = FastAPI()
 
 
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
 @app.get("/", response_class=HTMLResponse)
